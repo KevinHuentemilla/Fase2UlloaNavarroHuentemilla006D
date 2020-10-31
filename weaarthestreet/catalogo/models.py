d4c7878fe02f6416cf1802ca32ff686c1e89a0b6
unknown <kevinhuentemilla@gmail.com>
2020-10-31 00:18:24 -0300

    WearTheStreet:Creando Pagina Inicio

--- weaarthestreet/catalogo/models.py
+++ weaarthestreet/catalogo/models.py
@@ -6,10 +6,13 @@ import uuid  #se utiliza para definir atributos claves o la PK
 
 class Marca(models.Model):
         marca = models.CharField(max_length=100, help_text='Ingrese la marca y el tipo de producto EJ: Nike, Zapatilla; adidas, poleron')
+		
         
     
         def __str__(self):
 	        return self.marca
+
+			
 
 class Producto(models.Model):
     
@@ -17,7 +20,10 @@ class Producto(models.Model):
 	    precio=models.CharField(max_length=50)
         
 	    descripcion = models.TextField(max_length=1000, help_text='Ingrese una descripcion')
-	    marca=models.ManyToManyField(Marca)
+	    marca=models.ManyToManyField(Marca)	
+		
+		
+		
         
        
 
@@ -27,4 +33,4 @@ class Producto(models.Model):
     
 	    def get_absolute_url(self):
 		    """Returns the url to access a detail record for this book."""
-		    return reverse('book-detail', args=[str(self.id)])
+		    return reverse('detalle-producto', args=[str(self.id)])
--- weaarthestreet/catalogo/views.py
+++ weaarthestreet/catalogo/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from . models import Producto,Marca
+from .models import Producto, Marca
 from django.views import generic
 # Create your views here.
 
 def index(request):
-    num_Productos=Producto.object.all().count()
-    num_Marca_tipo=Marca.object.all().count()
+    
+    num_Marca_tipo=Marca.objects.all().count()
+    num_Productos=Producto.objects.all().count()
+    
 
     return render(
         request,
         'index.html',
-        context={'num_productos': num_Productos, 'num_marca': num_Marca_Tipo},
+        context={'num_productos': num_Productos, 'num_marca': num_Marca_tipo},
     )
+
+
+    
